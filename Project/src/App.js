bf0229bc1e608a8afc9b11c8f3e31c6b74edf1c1
Iuky.O <iumyfarias03@gmail.com>
2024-09-08 18:14:04 -0300

    Corpinho

---
+++ Project/src/App.js
@@ -0,0 +1,11 @@
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>aqui é a home... o rsto é historia kskks</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Project/src/Utils/Colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+    Rosa: '#F2A2D6',
+    Roxo: '#BA71D9',
+    Azul: '#94E1F2',
+    Verde: '#94FD8E',
+    Amarelo: '#F2C879',
+    Preto: '#0D0D0D',
+    Branco: '#FFFFFF'
+}
+
+export default Colors;
