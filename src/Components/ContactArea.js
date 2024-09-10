4adf6eedc2dd7c6eda0d2b3972a6bffe375b84b3
EduardoTrikk <154011898+EduardoTrikk@users.noreply.github.com>
2024-09-10 11:47:56 -0300

    Area de contato criada

--- src/Components/ContactArea.js
+++ src/Components/ContactArea.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "../Styles/ContactArea.css";
-import img from "../Assets/iameg.png";
+import gmail from "../Assets/gmailPreto.png";
+import whatsapp from "../Assets/whatsappPreto.png";
 
 const ContactArea = () => {
     return (
         <footer className="contact-area">
             <div className="contact-content">
                 <div className="text-section">
-                    <h2>Entre em contato</h2>
+                    <h1>Entre em contato</h1>
+                    <p>Em caso de dúvida, sugestão ou queira saber mais sobre o Simpósio Regional de Engenharia de Software entre em contato conosco</p>
                 </div>
                 <div className="social-section">
                     <div className="social-icon-footer">
-                        <a href="#"><img src={""} alt="Social Icon" /></a>
-                        <a href="#"><img src={""} alt="Social Icon" /></a>
-                        <a href="#"><img src={""} alt="Social Icon" /></a>
+                        <a href="#"><img src={gmail} alt="Gmail" /></a>
+                        <a href="#"><img src={whatsapp} alt="Whatsapp" /></a>
                     </div>
                 </div>
             </div>
