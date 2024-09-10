7930a0993156c5273a4398a755e923f0fe98a369
Iuky.O <iumyfarias03@gmail.com>
2024-09-10 12:04:49 -0300

    Home iniciada e precisando de avalizção

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -25,6 +25,7 @@ const Home = () => {
                     
                         <BsArrowRightCircleFill />  
                     </div>
+
                 </div>
                 
                 </div>
