ee39034ecfa6c54d4ce119b9099c8de260c28e72
Iuky.O <iumyfarias03@gmail.com>
2024-09-12 23:10:45 -0300

    Feito a tela sobre o curso

---
+++ src/Data/CoursesData.js
@@ -0,0 +1,70 @@
+import img from "../Assets/computador1.png";
+import img2 from "../Assets/computador2.png";
+import img3 from "../Assets/computador3.png";
+import img4 from "../Assets/computador4.png";
+import img5 from "../Assets/computador5.png";
+import img6 from "../Assets/computador6.png";
+import img7 from "../Assets/computador7.png";
+import img8 from "../Assets/computador8.png";
+import img9 from "../Assets/computador9.png";
+import img10 from "../Assets/computador10.png";
+
+
+export const Options_Courses = [
+    {
+      id: 1,
+      title: "Desenvolvimento Web",
+      description: "Descrição",
+      imgUrl: img,
+      days: 'No 3°, 4° e 5° dia'
+    },
+    {
+      id: 2,
+      title: "Arquitetura de Rede",
+      description: "Descrição",
+      imgUrl: img2,
+      days: 'No 1°, 2° e 3° dia'
+    },
+    {
+      id: 3,
+      title: "Análise e Projeto de Software",
+      description: "Descrição",
+      imgUrl: img3,
+      days: 'No 1°, 2° e 3° dia'
+    },
+    {
+      id: 4,
+      title: "Manutenção de Computadores",
+      description: "Descrição",
+      imgUrl: img4,
+      days: 'No 1° e 3° dia'
+    },
+    {
+      id: 5,
+      title: "Engenharia de Computação",
+      description: "Descrição",
+      imgUrl: img5,
+      days: 'No 2° e 3° dia'
+    },
+    {
+      id: 6,
+      title: "Engenharia de Software",
+      description: "Descrição",
+      imgUrl: img6,
+      days: 'No 4° dia'
+    },
+    {
+      id: 7,
+      title: "Engenharia de Requisitos",
+      description: "Descrição",
+      imgUrl: img7,
+      days: 'No 4° e 5° dia'
+    },
+    {
+      id: 8,
+      title: "Análise de Sistemas",
+      description: "Descrição",
+      imgUrl: img8,
+      days: ' No 5° dia'
+    }
+  ];
