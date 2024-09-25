2652973f46c322ad250e2bcd09152a8d03676e4b
Iuky.O <iumyfarias03@gmail.com>
2024-09-24 23:49:36 -0300

    ARRUMEI A TELA HOME, MAS NÃO TENHO CETEZA

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,30 +10,29 @@ const Home = () => {
         <div className="home">
       
             <div className="home-banner-container">
-                <div className="home-bannerImage-container">
-                <img src="{BannerBackground}" alt="" />
-                </div>
 
                 <div className="home-text-section">
-                <h1 className="heading-home">
-                    I Simpósio Regional de <span>Engenharia de Software</span>
-                </h1>
-                <p className="primary-text">
-                   Embarque nessa jornada com a gente!
-                </p>
-                <Link to="/register" className="btn">
-                    <div className="info-btn">
-                      <h6>Inscreva-se</h6>
-                    
-                        <BsArrowRightCircleFill />  
-                    </div>
+                    <h1 className="heading-home">
+                        I Simpósio Regional de <span>Engenharia de Software</span>
+                    </h1>
+                    <p className="primary-text">
+                    Embarque nessa jornada com a gente!
+                    </p>
+                    <Link to="/register" className="btn">
+                        <div className="info-btn">
+                        <h6>Inscreva-se</h6>
+                        
+                            <BsArrowRightCircleFill />  
+                        </div>
 
-                </Link>
+                    </Link>
                 
                 </div>
+                {/**
                 <div className="home-image-section">
-                <img src={Image} alt="" />
+                
                 </div>
+                 */}
             </div>
         </div>
     )
