b4a938c305cf9dc17b181f0580759b61441c9012
Leh826 <dev.rocha826@gmail.com>
2024-09-24 22:48:00 -0300

    Realizado Front-end aplication profile e back requisições

--- src/Components/RoutesComponent.jsx
+++ src/Components/RoutesComponent.jsx
@@ -13,6 +13,7 @@ import Collaborators from './Collaborators';
 import Lectures from './Lectures';
 import AboutLectures from './AboutLectures';
 import Register from './Register';
+import Profile from './UserPage';
 
 function RoutesComponent() {
   return (
@@ -23,13 +24,14 @@ function RoutesComponent() {
       <Route path="/enrollment" element={<EnrollmentArea />} />
       <Route path="/timeline" element={<TimelineArea />} />
       <Route path="/speakers" element={<Speakers />} />
-      <Route path="/aboutspeaker/:id" element={<AboutSpeaker />} /> 
-      <Route path="/aboutscourses/:id" element={<AboutCourses />} /> 
-      <Route path="/aboutslectures/:id" element={<AboutLectures />} /> 
+      <Route path="/aboutspeaker/:id" element={<AboutSpeaker />} />
+      <Route path="/aboutscourses/:id" element={<AboutCourses />} />
+      <Route path="/aboutslectures/:id" element={<AboutLectures />} />
       <Route path="/about/" element={<About />} />
       <Route path="/collaborators" element={<Collaborators />} />
-      <Route path= "/lectures" element={<Lectures/>} /> 
-      <Route path= "/register" element={<Register/>} /> 
+      <Route path="/lectures" element={<Lectures />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
