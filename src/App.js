d03c17cb8e0f49478d1dbc1c3b5248386d6d4c59
EduardoTrikk <154011898+EduardoTrikk@users.noreply.github.com>
2024-09-26 10:10:37 -0300

    Area do admin criada

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponent from "./Components/RoutesComponent"
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import {Admin, Resource, ShowGuesser } from 'react-admin';
+//import jsonServerProvider from 'ra-data-json-server';
+//import { dataProvider } from './Admin/dataProvider.js';
+import { UserList } from './Admin/users.tsx';
+import { PostList } from "./Admin/posts.tsx";
+import { Dashboard } from './Admin/Dashboard.tsx';
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
+//const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   
-  return (
+  return ( 
     <Router>
       <div className="App">
         <Navbar />
@@ -19,3 +29,11 @@ function App() {
 }
 
 export default App;
+
+/*
+
+      <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+        <Resource name="posts" list={PostList}  icon={PostIcon} />
+        <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon} />
+      </Admin>
+*/
