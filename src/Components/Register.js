c8712ee60a888f4ce7d7dbbdd20efd425c72c1f1
Iuky.O <iumyfarias03@gmail.com>
2024-10-02 12:22:44 -0300

    Organizei o cadastro, mas ainda vou ver a api

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,68 +1,90 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "../Styles/RegisterStyles.css";
 import Image from "../Assets/Open Peeps - Bust.png";
 import api from '../../src/service/api';
 
 const Register = () => {
+    // Estados para armazenar os valores dos campos
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [email, setEmail] = useState("");
+    const [numero, setNumero] = useState("");
+    const [pais, setPais] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [tipoUsuario, setTipoUsuario] = useState("");
+    const [vinculo, setVinculo] = useState("");
+    const [escolaridade, setEscolaridade] = useState("");
+    const [sexo, setSexo] = useState("");
+    const [nomeSocial, setNomeSocial] = useState("");
+    const [password, setPassword] = useState("");
 
-    const inputNome = useRef(null);
-    const inputSobrenome = useRef(null);
-    const inputEmail = useRef(null);
-    const inputNumero = useRef(null);
-    const inputPais = useRef(null);
-    const inputEstado = useRef(null);
-    const inputCidade = useRef(null);
-    const inputTipoUsuario = useRef(null);
-    const inputVinculo = useRef(null);
-    const inputEscolaridade = useRef(null);
-    const inputSexo = useRef(null);
-    const inputNomeSocial = useRef(null); // Esse campo é opcional
-    const inputPassword = useRef(null);
+    const [step, setStep] = useState(1); // Estado para controlar o passo atual
 
-    // Função de validação
-    const validateFields = () => {
-        if (
-            !inputNome.current?.value || 
-            !inputSobrenome.current?.value || 
-            !inputEmail.current?.value || 
-            !inputNumero.current?.value || 
-            !inputPais.current?.value || 
-            !inputEstado.current?.value || 
-            !inputCidade.current?.value || 
-            !inputTipoUsuario.current?.value || 
-            !inputVinculo.current?.value || 
-            !inputEscolaridade.current?.value || 
-            !inputSexo.current?.value || 
-            !inputPassword.current?.value
-        ) {
+    // Função para exibir dados preenchidos no console
+    const logFieldValues = () => {
+        console.log(`Passo ${step} atual:`);
+        console.log(`Nome: ${nome}`);
+        console.log(`Sobrenome: ${sobrenome}`);
+        console.log(`Email: ${email}`);
+        console.log(`Número: ${numero}`);
+        console.log(`País: ${pais}`);
+        console.log(`Estado: ${estado}`);
+        console.log(`Cidade: ${cidade}`);
+        console.log(`Tipo de Usuário: ${tipoUsuario}`);
+        console.log(`Vínculo: ${vinculo}`);
+        console.log(`Escolaridade: ${escolaridade}`);
+        console.log(`Sexo: ${sexo}`);
+        console.log(`Nome Social: ${nomeSocial || 'N/A'}`);
+        console.log(`Senha: ${password}`);
+        console.log("-----------------------");
+    };
+
+     // Função de validação
+     const validateFields = () => {
+        if (!nome || 
+            !sobrenome || 
+            !email || 
+            !numero || 
+            !pais || 
+            !estado || 
+            !cidade || 
+            !tipoUsuario || 
+            !vinculo || 
+            !escolaridade || 
+            !sexo || 
+            !password) {
             alert("Por favor, preencha todos os campos obrigatórios.");
             return false;
         }
         return true;
     };
 
+    // Função para criar usuário
     async function createUsers() {
-        // Validação de campos antes de enviar para a API
+
         if (!validateFields()) {
             return;
         }
 
-        // Dados estão validados, podemos enviar para a API
+        logFieldValues();
+
         try {
+        
             await api.post('/add-users', {
-                nome: inputNome.current?.value,
-                Sobrenome: inputSobrenome.current?.value,
-                email: inputEmail.current?.value,
-                numero: inputNumero.current?.value,
-                pais: inputPais.current?.value,
-                cidade: inputCidade.current?.value,
-                estado: inputEstado.current?.value,
-                tipoUsuario: inputTipoUsuario.current?.value,
-                vinculo: inputVinculo.current?.value,
-                escolaridade: inputEscolaridade.current?.value,
-                sexo: inputSexo.current?.value,
-                nomeSocial: inputNomeSocial.current?.value || '', // Nome social pode ser vazio
-                password: inputPassword.current?.value,
+                Nome: nome,
+                Sobrenome: sobrenome,
+                Email: email,
+                Numero: numero,
+                Pais: pais,
+                Cidade: cidade,
+                Estado: estado,
+                TipoUsuario: tipoUsuario,
+                Vinculo: vinculo,
+                Escolaridade: escolaridade,
+                Sexo: sexo,
+                NomeSocial: nomeSocial || '', // Nome social pode ser vazio
+                Password: password,
             });
             alert('Cadastro realizado com sucesso!');
         } catch (error) {
@@ -71,20 +93,21 @@ const Register = () => {
         }
     }
 
-    const [step, setStep] = useState(1); // Estado para controlar o passo atual
-
+    // Função para passar para o próximo passo
     const handleNextStep = () => {
+        logFieldValues(); // Log dos valores antes de mudar de passo
         setStep((prevStep) => Math.min(prevStep + 1, 4)); // Limita o passo máximo em 4
     };
 
+    // Função para voltar ao passo anterior
     const handlePrevStep = () => {
+        logFieldValues(); // Log dos valores antes de voltar de passo
         setStep((prevStep) => Math.max(prevStep - 1, 1)); // Limita o passo mínimo em 1
     };
 
     return (
         <div className="register">
-            <div className="background-rotated">
-            </div>
+            <div className="background-rotated"></div>
             <div className="container-register">
                 <div className="sections-container">
                     <div className="section-info">
@@ -93,40 +116,40 @@ const Register = () => {
                         </div>
                     </div>
                     <div className="section-form">
-                        <form className="form-cadastro" >
+                        <form className="form-cadastro">
                             <h1>Cadastro</h1>
                             <p>Faça seu cadastro para participar do evento!</p>
 
+                            {/* Formulário dividido em etapas */}
                             {step === 1 && (
                                 <>
                                     <div>
                                         <label htmlFor="nome">Nome:</label>
                                         <div className="input-group">
-                                            <input type="text" id="nome" name="nome" required ref={inputNome} />
+                                            <input type="text" id="nome" name="nome" required value={nome} onChange={(e) => setNome(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="sobrenome">Sobrenome:</label>
                                         <div className="input-group">
-                                            <input type="text" id="sobrenome" name="sobrenome" required ref={inputSobrenome} />
+                                            <input type="text" id="sobrenome" name="sobrenome" required value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="email">Email:</label>
                                         <div className="input-group">
-                                            <input type="text" id="email" name="email" required ref={inputEmail} />
+                                            <input type="text" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="numero">Número:</label>
                                         <div className="input-group">
-                                            <input type="text" id="numero" name="numero" required ref={inputNumero} />
+                                            <input type="text" id="numero" name="numero" required value={numero} onChange={(e) => setNumero(e.target.value)} />
                                         </div>
                                     </div>
-
                                 </>
                             )}
 
@@ -135,21 +158,21 @@ const Register = () => {
                                     <div>
                                         <label htmlFor="pais">País:</label>
                                         <div className="input-group">
-                                            <input type="text" id="pais" name="pais" required ref={inputPais} />
+                                            <input type="text" id="pais" name="pais" required value={pais} onChange={(e) => setPais(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="estado">Estado:</label>
                                         <div className="input-group">
-                                            <input type="text" id="estado" name="estado" required ref={inputEstado} />
+                                            <input type="text" id="estado" name="estado" required value={estado} onChange={(e) => setEstado(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="cidade">Cidade:</label>
                                         <div className="input-group">
-                                            <input type="text" id="cidade" name="cidade" required ref={inputCidade} />
+                                            <input type="text" id="cidade" name="cidade" required value={cidade} onChange={(e) => setCidade(e.target.value)} />
                                         </div>
                                     </div>
                                 </>
@@ -160,7 +183,7 @@ const Register = () => {
                                     <div>
                                         <label htmlFor="tipoUsuario">Tipo de Usuário:</label>
                                         <div className="input-group">
-                                            <select id="tipoUsuario" name="tipoUsuario" required ref={inputTipoUsuario}>
+                                            <select id="tipoUsuario" name="tipoUsuario" required value={tipoUsuario} onChange={(e) => setTipoUsuario(e.target.value)}>
                                                 <option value="">Selecione</option>
                                                 <option value="aluno">Aluno</option>
                                                 <option value="professor">Professor</option>
@@ -173,14 +196,14 @@ const Register = () => {
                                     <div>
                                         <label htmlFor="vinculo">Vínculo (Instituição ou Empresa):</label>
                                         <div className="input-group">
-                                            <input type="text" id="vinculo" name="vinculo" required ref={inputVinculo} />
+                                            <input type="text" id="vinculo" name="vinculo" required value={vinculo} onChange={(e) => setVinculo(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="escolaridade">Nível de Escolaridade:</label>
                                         <div className="input-group">
-                                            <input type="text" id="escolaridade" name="escolaridade" required ref={inputEscolaridade} />
+                                            <input type="text" id="escolaridade" name="escolaridade" required value={escolaridade} onChange={(e) => setEscolaridade(e.target.value)} />
                                         </div>
                                     </div>
                                 </>
@@ -191,7 +214,7 @@ const Register = () => {
                                     <div>
                                         <label htmlFor="sexo">Sexo:</label>
                                         <div className="input-group">
-                                            <select id="sexo" name="sexo" required ref={inputSexo}>
+                                            <select id="sexo" name="sexo" required value={sexo} onChange={(e) => setSexo(e.target.value)}>
                                                 <option value="">Selecione</option>
                                                 <option value="masculino">Masculino</option>
                                                 <option value="feminino">Feminino</option>
@@ -203,19 +226,20 @@ const Register = () => {
                                     <div>
                                         <label htmlFor="nomeSocial">Nome Social (Opcional):</label>
                                         <div className="input-group">
-                                            <input type="text" id="nomeSocial" name="nomeSocial" ref={inputNomeSocial} />
+                                            <input type="text" id="nomeSocial" name="nomeSocial" value={nomeSocial} onChange={(e) => setNomeSocial(e.target.value)} />
                                         </div>
                                     </div>
 
                                     <div>
                                         <label htmlFor="senha">Senha:</label>
                                         <div className="input-group">
-                                            <input type="password" id="senha" name="senha" required ref={inputPassword} />
+                                            <input type="password" id="senha" name="senha" required value={password} onChange={(e) => setPassword(e.target.value)} />
                                         </div>
                                     </div>
                                 </>
                             )}
 
+                            {/* Botões de navegação */}
                             <div className="navigation-buttons">
                                 {step > 1 && (
                                     <button type="button" onClick={handlePrevStep}>
@@ -227,22 +251,12 @@ const Register = () => {
                                         Próximo
                                     </button>
                                 ) : (
-                                    <button type="button" className="btn-cadastro" onClick={createUsers}>
-                                        Cadastrar-se
+                                    <button type="button" onClick={createUsers}>
+                                        Finalizar Cadastro
                                     </button>
                                 )}
                             </div>
                         </form>
-
-                        {/* Indicador de Progresso */}
-                        <div className="progress-indicator">
-                            {[1, 2, 3, 4].map((num) => (
-                                <span
-                                    key={num}
-                                    className={`dot ${step === num ? 'active' : ''}`}
-                                ></span>
-                            ))}
-                        </div>
                     </div>
                 </div>
             </div>
